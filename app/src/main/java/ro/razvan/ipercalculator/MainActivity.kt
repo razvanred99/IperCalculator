fc80d87700838875ec4501c8afdac5b122458bae
Razvan <razvanred99@gmail.com>
2018-04-04 21:35:21 +0200

    Created land/activity_main.xml Finished ResultActivity Rotation is now "handled" in MainActivity

--- app/src/main/java/ro/razvan/ipercalculator/MainActivity.kt
+++ app/src/main/java/ro/razvan/ipercalculator/MainActivity.kt
@@ -1,60 +1,112 @@
 package ro.razvan.ipercalculator
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
-import android.widget.TextView
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val operators=ArrayList<Char>()
-    private val operands=ArrayList<Float>()
+    private val TAG = MainActivity::class.java.simpleName
+
+    private val operators = ArrayList<Char>()
+    private val operands = ArrayList<Float>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "arraySize: ${operators.size} ${operands.size}")
+        Log.d(TAG, "CREATING")
     }
 
-    fun getResult(view: View){
-        val int= Intent(this,ResultActivity::class.java)
-        int.putExtra(getString(R.string.operands),operands.toArray())
-        int.putExtra(getString(R.string.operators),operators.toArray())
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+
+        outState?.putString("txvActualNumber", txvActualNumber.text.toString())
+        outState?.putString("txvOperations", txvOperations.text.toString())
+        outState?.putCharArray(getString(R.string.operators), operators.toCharArray())
+        outState?.putFloatArray(getString(R.string.operands), operands.toFloatArray())
+
+        Log.d(TAG, "arraySize: ${operators.size} ${operands.size}")
+        Log.d(TAG, "SAVING")
     }
 
-    fun clear(view:View){
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        txvActualNumber.text = savedInstanceState?.getString("txvActualNumber")
+        txvOperations.text = savedInstanceState?.getString("txvOperations")
+
+
+        val arr = savedInstanceState?.getCharArray(getString(R.string.operators))
+        if (arr != null) {
+            for (i in 0 until arr.size)
+                operators.add(arr[i])
+        }
+
+        val arr2 = savedInstanceState?.getFloatArray(getString(R.string.operands))
+        if (arr2 != null) {
+            for (i in 0 until arr2.size)
+                operands.add(arr2[i])
+        }
+
+        Log.d(TAG, "arraySize: ${operators.size} ${operands.size}")
+        Log.d(TAG, "RECREATING")
+    }
+
+
+    fun getResult(view: View) {
+        val int = Intent(this, ResultActivity::class.java)
+
+        operands.add(txvActualNumber.text.toString().toFloat())
+
+        int.putExtra(getString(R.string.operands), operands.toFloatArray())
+        int.putExtra(getString(R.string.operators), operators.toCharArray())
+
+        startActivity(int)
+    }
+
+    fun clear(view: View) {
         operStatus(true)
-        txvOperations.text=""
+        txvOperations.text = ""
+        txvActualNumber.text = "0"
+        btnEquals.isEnabled = false
         operators.clear()
         operands.clear()
     }
 
-    fun clickKeyboard(view:View){
-        val text=(view as TextView).text.toString()
-        val actualNumber=txvActualNumber.text.toString()
+    fun clickKeyboard(view: View) {
 
-        try{
-            if(txvActualNumber.text.toString().toInt()==0)
-                txvActualNumber.text = text
-            else
-                txvActualNumber.text=actualNumber+text
+        val textButton = (view as Button).text.toString()
+        val actualNumber = txvActualNumber.text.toString()
 
-        }catch(exc:NumberFormatException){
-            txvOperations.text=actualNumber+text
-            operators.add(text[0])
+        try {
+            if (actualNumber.toInt() == 0) {
+                txvActualNumber.text = textButton
+                operStatus(true)
+            } else
+                txvActualNumber.text = actualNumber + textButton.toInt()
+
+        } catch (exc: NumberFormatException) {
+            txvOperations.text = txvOperations.text.toString() + actualNumber + textButton
+            operators.add(textButton[0])
             operands.add(actualNumber.toFloat())
             operStatus(false)
+
+            btnEquals.isEnabled = true
+            txvActualNumber.text = "0"
         }
     }
 
-    private fun operStatus(boolean: Boolean){
-        btnPlus.isEnabled=boolean
-        btnMinus.isEnabled=boolean
-        btnFor.isEnabled=boolean
-        btnDivided.isEnabled=boolean
-
-        btnEquals.isEnabled=!boolean
-        txvActualNumber.text="0"
+    private fun operStatus(boolean: Boolean) {
+        btnPlus.isEnabled = boolean
+        btnMinus.isEnabled = boolean
+        btnFor.isEnabled = boolean
+        btnDivided.isEnabled = boolean
     }
+
 }
--- app/src/main/java/ro/razvan/ipercalculator/ResultActivity.kt
+++ app/src/main/java/ro/razvan/ipercalculator/ResultActivity.kt
@@ -1,12 +1,48 @@
 package ro.razvan.ipercalculator
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        setTitle(R.string.result)
+
+        val operators = intent.getCharArrayExtra(getString(R.string.operators))
+        val operands = intent.getFloatArrayExtra(getString(R.string.operands))
+
+        var result = 0f
+
+        for (i in 0 until operands.size)
+            if (i > 0)
+                result = interpreter(result, operands[i], operators[i - 1])
+            else
+                result = interpreter(result, operands[i], '+')
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        txvResult.text = result.toString()
+    }
+
+    private fun interpreter(o1: Float, o2: Float, char: Char) = when (char) {
+        '+' -> o1 + o2
+        '-' -> o1 - o2
+        '*' -> o1 * o2
+        '/' -> o1 / o2
+        else -> 0f
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when (item?.itemId) {
+            android.R.id.home -> finish()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
